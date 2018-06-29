04379db5056c1dc823563da0b3819787cf164e18
lotapp <39723758+lotapp@users.noreply.github.com>
2018-06-29 08:58:39 +0800

    en

--- life/en/sytle/js/nav.js
+++ life/en/sytle/js/nav.js
@@ -1,7 +1,7 @@
 $(function () {
     // 生成目录索引列表
     var temp_html = '<a name="dnttop"></a><div class="panel panel-info"><div class="panel-heading"><h4 class="panel-title">目录</h4></div><div class="panel-body">{dntnav}</div></div>';
-    var h_list = $('.container h2,.container h3');//这边你可以自行修改
+    var h_list = $('.container h1,.container h2,.container h3');//这边你可以自行修改
 
     if (h_list.length > 0) {
         var content = '<nav>';
@@ -12,10 +12,10 @@ $(function () {
             //Jupter-NoteBook
             var targetName = h_list[i].tagName.toLowerCase();
             var li2_content = '';
-            if (targetName == 'h3') {
-                li2_content = '<li style="padding-left: 1em;"><a href="#_map' + i + '">' + $(h_list[i]).text() + '</a></li>';
-            } else {
+            if (targetName == 'h1') {
                 li2_content = '<li><a href="#_map' + i + '">' + $(h_list[i]).text() + '</a></li>';
+            } else {
+                li2_content = '<li style="padding-left: 1em;"><a href="#_map' + i + '">' + $(h_list[i]).text() + '</a></li>';
             }
             content += li2_content;
         }
@@ -36,7 +36,7 @@ $(function () {
     }
     //发音
     $('code').attr('title', '发音请单击').click(function () {
-        console.log($(this).text());
+        // console.log($(this).text());
         $('body').append('<audio src="http://dict.youdao.com/dictvoice?audio=' + $(this).text() + '" autoplay>你的浏览器不支持</audio>');
     });
 });
