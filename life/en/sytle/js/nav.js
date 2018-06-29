6eba1724627fb63952d0709084b78d415e8731cb
lotapp <39723758+lotapp@users.noreply.github.com>
2018-06-29 08:01:07 +0800

    en

---
+++ life/en/sytle/js/nav.js
@@ -0,0 +1,42 @@
+$(function () {
+    // 生成目录索引列表
+    var temp_html = '<a name="dnttop"></a><div class="panel panel-info"><div class="panel-heading"><h4 class="panel-title">目录</h4></div><div class="panel-body">{dntnav}</div></div>';
+    var h_list = $('.container h2,.container h3');//这边你可以自行修改
+
+    if (h_list.length > 0) {
+        var content = '<nav>';
+        content += '<ul class="list-unstyled">';
+        for (var i = 0; i < h_list.length; i++) {
+            var go_to_top = '<a name="_map' + i + '"></a>';
+            $(h_list[i]).before(go_to_top);
+            //Jupter-NoteBook
+            var targetName = h_list[i].tagName.toLowerCase();
+            var li2_content = '';
+            if (targetName == 'h3') {
+                li2_content = '<li style="padding-left: 1em;"><a href="#_map' + i + '">' + $(h_list[i]).text() + '</a></li>';
+            } else {
+                li2_content = '<li><a href="#_map' + i + '">' + $(h_list[i]).text() + '</a></li>';
+            }
+            content += li2_content;
+        }
+        content += '</ul>';
+        content += '</nav>';
+        if ($('.container').length != 0) {
+            $($('.container')[0]).prepend(temp_html.replace("{dntnav}", content));
+        }
+
+        //返回顶部
+        var div_cells = $('.table-responsive');
+        var top_str = '<div style="text-align: right"><p><a href="#dnttop">返回顶部</a></p></div>';
+        for (var i = 0; i < div_cells.length;) {
+            i += 2;
+            $(div_cells[i]).append(top_str);
+        }
+        $('.container').append(top_str)
+    }
+    //发音
+    $('code').attr('title', '发音请单击').click(function () {
+        console.log($(this).text());
+        $('body').append('<audio src="http://dict.youdao.com/dictvoice?audio=' + $(this).text() + '" autoplay>你的浏览器不支持</audio>');
+    });
+});
