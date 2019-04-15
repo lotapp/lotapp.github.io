62faa433aada4efcc3c4781096d55fa9df332cd6
unknown <39723758+lotapp@users.noreply.github.com>
2019-04-15 23:30:26 +0800

    pull h5blog demo

--- html5/style/js/base.js
+++ html5/style/js/base.js
@@ -44,9 +44,10 @@ window.onload = function () {
         window.ScrollReveal({
             // easing: "ease", // 动画效果
             origin: "right", // 从右边出现
-            duration: "1500", // 持续时间1.5s
+            duration: 1200, // 持续时间1.5s
+            opacity: 0.2, // 最低透明度
             reset: true // 循环
-        }).reveal(".bloglist li,#header,#banner,.bg_white");
+        }).reveal(".bloglist li,.bg_white,#header,#banner,#footer");
         // 经测试，支持CSS选择器
     }
 }
