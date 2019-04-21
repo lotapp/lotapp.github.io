017c8e36d32a81c1242226e8ceeaa1c32572c9cd
unknown <39723758+lotapp@users.noreply.github.com>
2019-04-21 15:23:40 +0800

    pull h5blog demo

--- html5/mobil/sytle/js/base.js
+++ html5/mobil/sytle/js/base.js
@@ -1,8 +1,10 @@
 // 1.根据屏幕动态调整html文档的字体（现在移动端都是这么干的）
 var set_font_size = function () {
-    var docElement = document.documentElement;
-    var clientWidthValue = docElement.clientWidth > 750 ? 750 : docElement.clientWidth;
-    docElement.style.fontSize = 20 * (clientWidthValue / 375) + 'px';
+    var dom = document.documentElement;
+    // 屏幕大于375的1.5倍就以375的1.5倍来计算（防止PC端头尾太高）
+    var clientWidthValue = dom.clientWidth > 562.5 ? 562.5 : dom.clientWidth;
+    // 375默认大小是20px，那就等比缩放
+    dom.style.fontSize = 20 * (clientWidthValue / 375) + 'px';
 }
 window.onresize = set_font_size;
 // 需要手动执行一次
